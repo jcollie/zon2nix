7b5973f0ca5b077354c273143a437e4f53bfc4d7
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-09-05 16:45:21 -0500

    simplify flake

--- flake.nix
+++ flake.nix
@@ -1,38 +1,47 @@
 {
-  description = "zig2nix flake";
+  description = "zig2nix";
 
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable-small/nixexprs.tar.xz";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      makePackages =
+        system:
+        import nixpkgs {
           inherit system;
         };
-      in {
-        devShells = {
-          default = self.devShells.${system}.zig_0_15;
-          zig_0_15 = pkgs.mkShell {
-            packages = [
-              pkgs.zig_0_15
-              pkgs.nix-prefetch-git
-              pkgs.nixfmt-rfc-style
-              pkgs.valgrind
-            ];
-          };
+      forAllSystems = (
+        function:
+        nixpkgs.lib.genAttrs [
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-linux"
+          "x86_64-darwin"
+        ] (system: function (makePackages system))
+      );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = self.devShells.${pkgs.system}.zig_0_15;
+        zig_0_15 = pkgs.mkShell {
+          packages = [
+            pkgs.zig_0_15
+            pkgs.nix-prefetch-git
+            pkgs.nixfmt-rfc-style
+            pkgs.valgrind
+          ];
         };
-        packages = {
-          #! zon2nix: Converts build.zig.zon to nix deriviation
-          zon2nix = pkgs.callPackage ./package.nix {};
-        };
-      }
-    );
+      });
+      packages = forAllSystems (pkgs: {
+        zon2nix = pkgs.callPackage ./package.nix { };
+      });
+    };
+
 }
