c698393aec639700933e01a7ea3474dc509720d4
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-11-08 15:45:10 -0600

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "zig2nix";
 
   inputs = {
-    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
   };
 
   outputs =
